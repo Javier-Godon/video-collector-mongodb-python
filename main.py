ff9b4857c938710633d10910c95b6e87c79a24cd
Javier Godon <javier.godon@gmail.com>
2024-12-20 12:34:55 +0100

    test: wip

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ port = get_data()['server']['port']
 # @app.get("/")
 # async def root():
 #     return {"message": "Hello Bigger Applications!"}
+#
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
