1198c4e445b820c9de7a04497e9a5c4dfb707d63
Javier Godon <javier.godon@gmail.com>
2024-12-25 18:21:00 +0100

    fix: router collector/videos fixed

--- app/usecases/get_full_video_by_sha_id/rest/get_full_video_by_video_id_router.py
+++ app/usecases/get_full_video_by_sha_id/rest/get_full_video_by_video_id_router.py
@@ -5,6 +5,6 @@ from app.usecases.get_full_video_by_sha_id.get_full_video_by_video_id_handler im
 router = APIRouter()
 
 
-@router.get("/collector/videos/sha_id")
+@router.get("/collector/videos/")
 async def collect_video_by_sha_id(sha_id: str):
     return await get_full_video_by_sha_id(sha_id=sha_id)
